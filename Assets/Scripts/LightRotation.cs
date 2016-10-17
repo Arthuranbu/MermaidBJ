4b4454872dd370d9e0b3c49b9a0244dffa4f7480
EthanChing <forfer@msn.com>
2016-10-17 13:52:16 -0700

    LightRotationNew

---
+++ Assets/Scripts/LightRotation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightRotation : MonoBehaviour
+{
+
+    public Transform target;
+    private float distance;
+    public float dropDistance =40;
+    public float smooth;
+    public float intensity;
+    public float maxIntensity = 1.56f;
+    private Light sunLight;
+    void Start()
+    {
+        sunLight = GetComponent<Light>();
+    }
+    void Update()
+    {
+        distance = Mathf.Round(Vector3.Distance(transform.position, target.position));
+        if (distance > dropDistance)
+        {
+            intensity = 0;
+        }
+        else
+        {
+            intensity = (1.0f - distance / dropDistance) * maxIntensity;
+        }
+        sunLight.intensity = Mathf.Lerp(sunLight.intensity, intensity, smooth * Time.deltaTime);
+    }
+}
