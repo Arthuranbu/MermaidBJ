40d3b0ce827a18c6ef45892ccf3c4dfd9aecd6d6
EthanChing <forfer@msn.com>
2016-10-27 22:17:38 -0700

    StartQuitLoadlevel
    ez

---
+++ Assets/Scripts/StartQuit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quit : MonoBehaviour
+{
+
+    public void onClickQuit()
+    {
+        Application.Quit();
+    }
+
+    public void onClickStart()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+    }
+}
