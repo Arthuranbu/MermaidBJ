539f3d742656b317331454de542353dac800cd5e
EthanChing <forfer@msn.com>
2016-11-01 21:45:25 -0700

    BigThingsPoppin
    Pop Pop

---
+++ Assets/Scripts/Mermaid.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mermaid : MonoBehaviour {
+
+    public GameObject Treasure;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+
+        {
+            UnityEngine.Animation.Play("");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+    }
+}
