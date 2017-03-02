8f781735b13eed6a72935dc9615d696fc4e68b64
EthanChing <forfer@msn.com>
2017-03-01 19:27:01 -0800

    CreditsScreen Done

--- Assets/Scripts/StartQuit.cs
+++ Assets/Scripts/StartQuit.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class StartQuit : MonoBehaviour
 {
     public GameObject ScoreKeeper;
+    public GameObject credits;
+    public GameObject creditholder;
+
+
     public void onClickQuit()
     {
         Application.Quit();
@@ -15,4 +19,14 @@ public class StartQuit : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
         Destroy (ScoreKeeper);
     }
+    public void onClickCredits()
+    {
+        credits = creditholder.transform.Find ("CreditsScreen").gameObject;
+        credits.SetActive(true);
+    }
+    public void onClickBack()
+    {
+        credits = GameObject.Find("CreditsScreen");
+        credits.SetActive(false);
+    } 
 }
